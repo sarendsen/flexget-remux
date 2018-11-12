831dfba41e08e385e56afe22612b1886ed885496
sarendsen <subscribe@optixdesigns.com>
2018-11-12 12:23:15 +0100

    Remote test file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     description='',
     packages=find_packages(exclude=['tests']),
-    #install_requires=['FlexGet>2.2'],
+    install_requires=['FlexGet>2.2'],
     tests_require=['pytest'],
     entry_points="""
         [FlexGet.plugins]
